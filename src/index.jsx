90b651497407def0fd4bca2c9ac99f1476f8c625
relative <relative@pxl.blue>
2021-01-26 01:24:56 -0500

    fix: unregister service workers

--- src/index.jsx
+++ src/index.jsx
@@ -13,15 +13,8 @@ import 'styles/index.scss'
 import pxlApi from 'pxl/Api'
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('SW registered', registration)
-      })
-      .catch((registrationError) => {
-        console.error('SW registration failed', registrationError)
-      })
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((reg) => reg.unregister())
   })
 }
 
